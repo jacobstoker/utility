9551e228136e243942d43d99e75c191421ee04b6
jacobstoker <jacob.stoker42@gmail.com>
2024-01-16 12:16:01 +0000

    - convert moisture content to g/g - modify dataframes in place rather than needlessly returning them

--- excel_dvs_data/excel_dvs_data.py
+++ excel_dvs_data/excel_dvs_data.py
@@ -4,8 +4,8 @@ import logging
 
 """Script to add required columns to an Excel Spreadsheet of DVS data"""
 
-SOURCE_SPREADSHEET = "DVS Heptane data.xlsx"
-DESTINATION_SPREADSHEET = "DVS Heptane data Updated.xlsx"
+SOURCE_SPREADSHEET = "DVS Water data Original.xlsx"
+DESTINATION_SPREADSHEET = "DVS Water data Updated.xlsx"
 
 logging.basicConfig(level=logging.INFO)
 
@@ -43,68 +43,66 @@ def check_for_needed_columns(df: pd.DataFrame, pressure_solvent_column: str) -> 
 
 def add_normalised_column(
     df: pd.DataFrame, grouping_column: str, target_column: str, new_column_name: str
-) -> pd.DataFrame:
+):
     """Add a column normalising target_column by changes in grouping_column"""
     df[f"{target_column}_at_change"] = df.groupby(grouping_column)[
         target_column
     ].transform("first")
-    df[f"{new_column_name} NEW"] = df[target_column] - df[f"{target_column}_at_change"]
+    df[new_column_name] = df[target_column] - df[f"{target_column}_at_change"]
     df.drop(columns=[f"{target_column}_at_change"], inplace=True)
-    return df
 
 
 def add_change_column(
     df: pd.DataFrame,
     target_column: str,
     new_column_name: str,
-) -> pd.DataFrame:
+):
     """
     Add a column to the DataFrame indicating a change in target_column, so you can properly group by the target_column without worrying about repeated values
     eg. target_column = A, B, B, A -> new_column_name = 1, 2, 2, 3
     """
     df[new_column_name] = (df[target_column] != df[target_column].shift()).cumsum()
-    return df
 
 
-def add_pressure_change_columns(
-    df: pd.DataFrame, pressure_solvent_column: str
-) -> pd.DataFrame:
+def convert_moisture_column(df: pd.DataFrame):
+    df["Moisture Content g/g"] = df["Moisture content %"] / (
+        100 - df["Moisture content %"]
+    )
+
+
+def add_pressure_change_columns(df: pd.DataFrame, pressure_solvent_column: str):
     """Add the columns based on pressure change-related to the DataFrame"""
-    df = add_change_column(df, pressure_solvent_column, "Pressure Change")
-    df = add_normalised_column(
+    add_change_column(df, pressure_solvent_column, "Pressure Change")
+    add_normalised_column(
         df, "Pressure Change", "Time [minutes]", "Normalised time (Individual RH Steps)"
     )
-    df = add_normalised_column(
+    convert_moisture_column(df)
+    add_normalised_column(
         df,
         "Pressure Change",
-        "Moisture content %",
+        "Moisture Content g/g",
         "Normalised moisture content per RH step",
     )
 
     df.drop(columns=["Pressure Change"], inplace=True)
-    return df
 
 
-def add_direction_change_columns(df: pd.DataFrame) -> pd.DataFrame:
+def add_direction_change_columns(df: pd.DataFrame):
     """Add the columns based on direction change to the DataFrame"""
-    df = add_change_column(df, "RH Direction", "Direction Change")
-    df = add_normalised_column(
+    add_change_column(df, "RH Direction", "Direction Change")
+    add_normalised_column(
         df,
         "Direction Change",
         "Time [minutes]",
         "Normalised time (up vs down RH ramps)",
     )
     df.drop(columns=["Direction Change"], inplace=True)
-    return df
 
 
-def calculate_and_add_columns(
-    df: pd.DataFrame, pressure_solvent_column: str
-) -> pd.DataFrame:
+def calculate_and_add_columns(df: pd.DataFrame, pressure_solvent_column: str):
     """Calculate and add all necessary columns to the DataFrame"""
-    df = add_pressure_change_columns(df, pressure_solvent_column)
-    df = add_direction_change_columns(df)
-    return df
+    add_pressure_change_columns(df, pressure_solvent_column)
+    add_direction_change_columns(df)
 
 
 def process_spreadsheet(src_spreadsheet: str, dst_spreadsheet: str):
@@ -129,7 +127,7 @@ def process_spreadsheet(src_spreadsheet: str, dst_spreadsheet: str):
             continue
 
         df = df.apply(pd.to_numeric, errors="ignore")
-        df = calculate_and_add_columns(df, pressure_solvent_column)
+        calculate_and_add_columns(df, pressure_solvent_column)
         df.to_excel(dst_workbook, sheet_name=sheet, index=False)
 
     if dst_workbook.sheets:
